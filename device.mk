cff22ce0bc9e0ddc8816c6bc8071869f2da3b4e6
KagehiraNE <ne@kagehira.icu>
2026-03-28 23:34:19 +1000

    debug config
    Change-Id: I47b83ddb5a6c104b31f7954262380d7f2085617e

--- device.mk
+++ device.mk
@@ -73,3 +73,7 @@ $(call inherit-product, device/oneplus/sm8750-common/common.mk)
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/pagani/pagani-vendor.mk)
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.adb.secure=0 \
+    persist.sys.usb.config=adb
